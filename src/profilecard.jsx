8cb86e13d4d61235a88d070676e9c22b466eb70c
Eduos0508 <eduos0508@gmail.com>
2025-03-25 18:50:01 -0700

    Agrege el componente profile

---
+++ src/profilecard.jsx
@@ -0,0 +1,9 @@
+ function Profilecard(title, handle, image) {
+    console.log(title)
+    console.log(handle)
+    console.log(image)
+  return (
+    <div>profilecard</div>
+  )
+}
+export default Profilecard
