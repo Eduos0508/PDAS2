ec948af2e267ff42beb53313402ae184c84e6c39
Eduos0508 <eduos0508@gmail.com>
2025-03-27 19:03:48 -0700

    hice un cambio ligero en Profilecard

--- src/profilecard.jsx
+++ src/profilecard.jsx
@@ -1,9 +1,19 @@
- function Profilecard(title, handle, image) {
-    console.log(title)
-    console.log(handle)
-    console.log(image)
+ function Profilecard({title, handle, image}) {
+
   return (
-    <div>profilecard32</div>
+   <div className="card">
+    <div className="card-image">
+      <figure className="image is-1by1">
+        <img src={image} alt="" width={200} height={200} />
+      </figure>
+    </div>
+    <div className="card-content">
+      <div className="media-content">
+        <p className="title is-4">tilte is {title}</p>
+        <p className="subtitle is-6">handle is{handle}</p>
+      </div>
+    </div>
+   </div>
   )
 }
 export default Profilecard
